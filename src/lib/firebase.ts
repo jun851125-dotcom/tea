02907f1231abd8bb4a1b7fcede51aa958d2bc3e1
jun851125-dotcom <jun851125@gmail.com>
2026-05-10 10:07:22 +0800

    更新 src/lib/firebase.ts via EZPage

---
+++ src/lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { initializeFirestore, doc, getDocFromServer } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+
+// Use experimentalForceLongPolling to improve connectivity in sandboxed environments
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+}, firebaseConfig.firestoreDatabaseId);
+
+export const auth = getAuth(app);
